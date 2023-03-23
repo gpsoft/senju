f3ad191866489aca1672cbb274f4bc7f38b40c2a
gpsoft <etoh.gpsoft@gmail.com>
2023-03-23 18:17:08 +0900

    Setup ring and bidi

--- src/clj/senju/core.clj
+++ src/clj/senju/core.clj
@@ -1,11 +1,64 @@
 (ns senju.core
   (:require
+   [bidi.ring :refer [make-handler ->Redirect]]
+   [ring.adapter.jetty :as jetty]
+   [ring.util.response :as res]
+   [clojure.java.io :as io]
    [senju.util :as u])
   (:gen-class))
 
+(def default-config
+  {:port 8080
+   })
+
+(defn- index
+  [_]
+  (res/response "Index!"))
+
+(defn- api-test
+  [_]
+  {:status 200
+   :headers {}
+   :body "Good"})
+
+(defn- not-found
+  [_]
+  {:status 404})
+
+(def routes
+  ["/" {"" (->Redirect 307 index)
+        "index.html" #'index
+        "api/" {"test" #'api-test}
+        true #'not-found}
+   ; "/resources" (->ResourcesMaybe {:prefix "public/"})
+   ; "pics/" (->Files {:dir "/tmp/pics"})
+   ])
+
+(defn config
+  []
+  (->> (u/read-edn "." "config.edn")
+       (merge default-config)))
+
+(defn application
+  []
+  (-> (make-handler routes)
+      #_wrap-hoge))
+
+(defn go
+  ([cfg] (go cfg false))
+  ([{:keys [port] :as cfg} from-main?]
+   (let [app (application)
+         opts {:port port
+               :join? false}]
+     (jetty/run-jetty app opts))))
+
 (defn -main [& args]
-  (println (u/tap "hey")))
+  (-> (config)
+      (go true)))
 
 (comment
- (-main)
+
+ (config)
+ (go (config))
+
  )
--- src/cljc/senju/util.cljc
+++ src/cljc/senju/util.cljc
@@ -1,3 +1,38 @@
-(ns senju.util)
+(ns senju.util
+  (:require
+   [clojure.java.io :as io]
+   [clojure.edn :as edn]))
 
 (defn tap [v] (prn v) v)
+
+(defn mac-os?
+  []
+  (let [os-name (System/getProperty "os.name")]
+    (.startsWith os-name "Mac")))
+
+(defn read-edn
+  [dir-path-str fname-str]
+  (let [d (io/file dir-path-str)
+        f-str (-> d
+                  (.toPath)
+                  (.resolve fname-str)
+                  (.toString))]
+    (if (.exists (io/file f-str))
+      (-> f-str
+          (slurp)
+          (edn/read-string))
+      {})))
+
+(defn move-ix
+  [ix n forward?]
+  (if (zero? n)
+    0
+    (mod ((if forward? inc dec) ix) n)))
+
+(defn scale-to-contain
+  [tow toh fromw fromh]
+  (let [rw (/ tow fromw)
+        rh (/ toh fromh)
+        r (min rw rh)]
+    [(int (* fromw r)) (int (* fromh r))]))
+
